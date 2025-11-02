f3004e80288eee159c77a280f44ce99e8d6601ef
Takashidfghjkhl <dmitrijvahrushev029@gmail.com>
2025-11-02 12:23:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,19 +9,23 @@ load_dotenv()
 password = os.getenv("PASSWORD")
 server = smtplib.SMTP_SSL('smtp.yandex.ru:465')
 msg = MIMEMultipart()
+
 msg["From"] = "dmitryvah029@yandex.ru"
 msg["To"] = "dmitryvah029@yandex.ru"
-msg["Subject"] = "hfgfufjghv"
+msg["Subject"] = "Мои занятия програмированием"
 process = ['Основы Python', 'GitHub', 'API']
 completed = ['Командная строка', 'Введение в Python', 'Введение в JS']
 
 time = "3 месяца"
-letter = f"Привет Мама(Папа), я занимаюсь в школе третье место уже {time}. В процессе я выполнил модули: {completed}! Сейчас я работаю над модулями {process}. Обучение мне нравится, я получил море знаний!"
+letter = f"Привет Мама(Папа), я занимаюсь в школе третье место уже {time}. "
+"В процессе я выполнил модули: {completed}! Сейчас я работаю над модулями {process}. "
+"Обучение мне нравится, я получил море знаний!" 
 if not completed: 
-    letter = f"Привет Мама(Папа), я занимаюсь в школе третье место уже {time}. Сейчас я работаю над модулями {process}. Пока что я улучшаю свои навыки и узнаю много нового!"
+    letter = f"Привет Мама(Папа), я занимаюсь в школе третье место уже {time}. "
+    "Сейчас я работаю над модулями {process}. "
+    "Пока что я улучшаю свои навыки и узнаю много нового!"
     
 msg.attach(MIMEText(letter, "plain"))
 server.login("dmitryvah029@yandex.ru", password)
 server.sendmail(msg["From"], msg["To"], msg.as_string())
 server.quit()
-
